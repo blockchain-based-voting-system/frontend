8ce94e41cb3fdacc31563d5468ce3c6db64e9084
AabhasDhaubanja <aabhas41@gmail.com>
2022-01-31 19:43:15 +0545

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
@@ -10,6 +11,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ textAlign: "center", paddingTop: 100, paddingBottom: 100 }}
+    >
+      <div>Copyright © 2022 BBVS. All rights reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
